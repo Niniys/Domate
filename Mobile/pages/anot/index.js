f350205f677e6e14acfc4fa3610ec015b52e9c62
Victória Rosa <85229966+vick7000@users.noreply.github.com>
2022-05-20 11:18:09 -0300

    Add files via upload

---
+++ Mobile/pages/anot/index.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Text, ToastAndroid, TouchableOpacity, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function Login({ navigation }) {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+    useEffect(async () => {
+        if(await AsyncStorage.getItem("userdata") !== null){
+            navigation.navigate("Main")
+        }
+
+    }, [])
+
+    const autenticar = () => {
+        let usuario = {
+            email: email,
+            senha: senha,
+        }
+
+        fetch('http://10.87.207.4:3000/login', {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": JSON.stringify(usuario),
+        })
+        .then(resp => { return resp.json() })
+        .then(async data => {
+            if(data.length > 0) {
+                await AsyncStorage.setItem('userdata', JSON.stringify(data[0]));
+                navigation.navigate('Anotacao');
+            }else {
+                ToastAndroid.show('Email ou Senha Invalidos', ToastAndroid.SHORT);
+            }
+        })
+    }
+}
---
+++ Mobile/pages/main/index.js
@@ -0,0 +1,117 @@
+import * as React from "react";
+import { View, TouchableOpacity, Image, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { LinearGradient } from "expo-linear-gradient";
+// import style from "./style.js";
+import Icon from "react-native-vector-icons/Entypo";
+
+import Anote from "../anot";
+import Timer from "../timer";
+
+function MyTabBar({ state, descriptors, navigation }) {
+  let selected = state.index;
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        let arrColor = ["#F51344", "#F0125C"];
+        let icon = (
+          <Icon
+            name="home"
+            color={selected == index ? "#FAD2D2" : "#000"}
+            size={30}
+          />
+        );
+        if (index == 1) {
+          arrColor = ["#F0125C", "#EB1075"];
+          icon = (
+            <Icon
+              name="circle-with-plus"
+              color={selected == index ? "#FAD2D2" : "#000"}
+              size={30}
+            />
+          );
+        } else if (index == 2) {
+          arrColor = ["#EB1075", "#E50F90"];
+          icon = (
+            <Icon
+              name="user"
+              color={selected == index ? "#FAD2D2" : "#000"}
+              size={26}
+            />
+          );
+        }
+
+        return (
+          <View key={index} style={{ flex: 1, height: 60, width: "100%" }}>
+            <LinearGradient
+              start={{ x: 0, y: 0.75 }}
+              end={{ x: 1, y: 0.25 }}
+              colors={arrColor}
+              style={{ flex: 1, height: 40, width: "100%" }}
+            >
+              <TouchableOpacity
+                accessibilityRole="button"
+                accessibilityState={isFocused ? { selected: true } : {}}
+                accessibilityLabel={options.tabBarAccessibilityLabel}
+                testID={options.tabBarTestID}
+                onPress={onPress}
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                {icon}
+              </TouchableOpacity>
+            </LinearGradient>
+          </View>
+        );
+      })}
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer independent={true}>
+      <StatusBar hidden style="light" translucent={false} />
+      <Tab.Navigator
+        tabBar={(props) => <MyTabBar {...props} />}
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#F51344",
+          },
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen name="Anote" component={Anote} />
+
+        <Tab.Screen name="Timer" component={Timer} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
