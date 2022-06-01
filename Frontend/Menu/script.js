35ebbdd4bb389e9523f077a483336f7edc4cffa3
Victória Rosa <vickrosa7@hotmail.com>
2022-06-01 10:57:30 -0300

    menu

--- Frontend/Menu/script.js
+++ Frontend/Menu/script.js
@@ -603,5 +603,30 @@ function cadastrar() {
     } else {
         alert("senhas devem ser iguais")
     }
-
 }
+
+
+var $btnAumentar = $("#btnAumentar");
+var $btnDiminuir = $("#btnDiminuir");
+var $elemento = $("body #content").find("*"); //encontra todos os elementos dentro do #content
+var fonts = [];
+
+function obterTamanhoFonte() {
+  for (var i = 0; i < $elemento.length; i++) {
+    fonts.push(parseFloat($elemento.eq(i).css('font-size')));
+  }
+}
+obterTamanhoFonte();
+$btnAumentar.on('click', function() {
+  for (var i = 0; i < $elemento.length; i++) {
+    ++fonts[i];
+    $elemento.eq(i).css('font-size', fonts[i]);
+  }
+});
+
+$btnDiminuir.on('click', function() {
+  for (var i = 0; i < $elemento.length; i++) {
+    --fonts[i];
+    $elemento.eq(i).css('font-size', fonts[i]);
+  }
+});
