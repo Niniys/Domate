9edc3a5592afe72867fa9f2671fea9bd22c0b50f
vick7000 <gabrielalexsandro2020@gmail.com>
2022-06-01 10:07:27 -0300

    terc

--- Frontend/timer/tarefa.js
+++ Frontend/timer/tarefa.js
@@ -1,28 +1,67 @@
+var botao = document.getElementById("add")
+var anote = document.getElementById("compromisso")
 document.addEventListener("keypress", function(e) {
     if (e.key === "Enter") {
         const add = document.getElementById("add");
         add.click();
     }
 })
+botao.addEventListener("click", () => {
+    let data = JSON.stringify({
+        anot: anote.value
+    })
+    fetch("http://10.87.207.4:3000/nota", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            "body": data
+        })
+        .then(response => { return response.json() })
+        .then(data => {
+            if (data.anot == "") {
+                alert("Erro ao adicionar uma anotação")
+            } else {
 
-function add() {
-    let corpo = document.querySelector(".corpo")
-    let card = document.createElement("div")
-    let comp = document.getElementById("compromisso")
-    card.className = "card"
-    card.innerHTML = "<label>"+comp.value+"</label>"
-    let importante = document.createElement("button")
-    importante.className = "importante"
-    importante.addEventListener("click", () => {
-        card.setAttribute("style","z-index: 5;background-color:#black ;border: 3px solid rgb(255, 0, 0);");
-    });
-    let excluir = document.createElement("button")
-    excluir.className = "excluir"
-    excluir.addEventListener("click", () => {
-        card.remove();
-    });
-    card.appendChild(importante)
-    card.appendChild(excluir)
-    corpo.appendChild(card)
-    comp.value = ""
-}
+                const host = localStorage.setItem("user", JSON.stringify(data));
+
+
+                let corpo = document.querySelector(".corpo")
+                let card = document.createElement("div")
+                let comp = document.getElementById("compromisso")
+                card.className = "card"
+                card.innerHTML = "<label>" + comp.value + "</label>"
+                let importante = document.createElement("button")
+                importante.className = "importante"
+                importante.addEventListener("click", () => {
+                    card.setAttribute("style", "z-index: 5;background-color:#black ;border: 3px solid rgb(255, 0, 0);");
+                });
+                let excluir = document.createElement("button")
+                excluir.className = "excluir"
+                excluir.addEventListener("click", () => {
+                    card.remove();
+                    let data = JSON.stringify({
+                        anot: anote.value
+                    })
+                    fetch("http://10.87.207.4:3000/nota/" + host.id, {
+                            method: "DELETE",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            "body": data
+                        })
+                        .then(response => { return response.json() })
+                        .then(data => {
+                            console.log(data)
+                        })
+                })
+                card.appendChild(importante)
+                card.appendChild(excluir)
+                corpo.appendChild(card)
+                comp.value = ""
+
+
+            }
+        })
+
+})
