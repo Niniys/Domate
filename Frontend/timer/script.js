5f8dee9ce62104af459b16594ec7b2d185c9e125
Victória Rosa <vickrosa7@hotmail.com>
2022-06-03 11:02:31 -0300

    timer cores

--- Frontend/timer/script.js
+++ Frontend/timer/script.js
@@ -210,3 +210,19 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   
   });
+
+var temas = ["Padrao", "Lilas", "Inverno", "RosaChoque", "Bege", "Roxo", "Limonada", "CeuLimpo"];
+var indexTema = -1;
+
+function loadtema() {
+  var temaSelecionado = parseInt(localStorage.getItem("temaselecionado"));
+  localStorage.setItem(temas[temaSelecionado], "enabled");
+  document.body.classList.add(temas[temaSelecionado]);
+}
+
+function setTema() {
+  var temaSelecionado = localStorage.getItem("temaselecionado");
+  if (temaSelecionado !== null) {
+      indexTema = Integer.parseInt(temaSelecionado);
+  }
+}
