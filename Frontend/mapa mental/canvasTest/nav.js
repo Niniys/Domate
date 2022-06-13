008a57622b532a40e6a5e8527312c922a91b9752
Michael-Medeiros <85230048+Michael-Medeiros@users.noreply.github.com>
2022-06-12 23:04:37 -0300

    Add files via upload

--- Frontend/mapa mental/canvasTest/nav.js
+++ Frontend/mapa mental/canvasTest/nav.js
@@ -1,19 +1,50 @@
+var body = document.querySelector("#body");
 var header = document.querySelector("#top");
+var nav = document.createElement("div");
+var label = document.createElement("div");
+var label1 = document.createElement("div");
+var label2 = document.createElement("div");
+var menu = document.createElement("div");
+nav.id = "menu";
+label.id = "label";
+label1.id = "label1";
+label2.id = "label2";
+menu.id = "back";
 
-function menuNav(){
-    var nav = document.createElement("div");
-    nav.id = "menu";
-    var lebel = document.createElement("div");
-    var lebel1 = document.createElement("div");
-    var lebel2 = document.createElement("div");
-    lebel.id = "lebel";
-    lebel1.id = "lebel1";
-    lebel2.id = "lebel2";
-    
-    header.appendChild(nav);
-    nav.appendChild(lebel);
-    nav.appendChild(lebel1);
-    nav.appendChild(lebel2);
+header.appendChild(nav);
+nav.appendChild(label2);
+nav.appendChild(label1);
+nav.appendChild(label);
+body.appendChild(menu);
+
+let x = 0;
+
+function menuNav() {
+  nav.addEventListener("click", () => {
+    if (x === 0) {
+      label2.classList.add("down");
+      label2.classList.remove("downReverse");
+      label.classList.add("rideUp");
+      label.classList.remove("rideUpReverse");
+      label1.classList.add("reduce");
+      label1.classList.remove("reduceReverse");
+      menu.classList.add("toRecall");
+      menu.classList.remove("appear");
+
+      nav.style.zIndex = 2;
+      x = 1;
+    } else {
+      label2.classList.remove("down");
+      label2.classList.add("downReverse");
+      label.classList.remove("rideUp");
+      label.classList.add("rideUpReverse");
+      label1.classList.remove("reduce");
+      label1.classList.add("reduceReverse");
+      menu.classList.remove("toRecall");
+      menu.classList.add("appear");
+
+      x = 0;
+    }
+  });
 }
-
 menuNav();
